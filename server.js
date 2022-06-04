3c75e7a4bf3bde0cdf68d38ca8e94b7cb19d6468
Manith De Silva <manith@Maniths-MacBook-Pro.local>
2022-06-04 18:49:31 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require ("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+    res.send("Remind Me Express Server");
+});
+
+app.post("/name", (req, res) => {
+    if(req.body.name){
+        return res.json({name: req.body.name});
+    } else {
+        return res.status(400).json({error: "No Name Provoded" });
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
